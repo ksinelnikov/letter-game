3d6d235793e84ec09d7b0d19bd25184a869b5d3b
ksinelnikov <ksin2042@gmail.com>
2024-01-22 22:25:30 +0100

    Update alphabet.js
    Cleaning up JavaScript code for public repo.

--- alphabet.js
+++ alphabet.js
@@ -1,11 +1,11 @@
-
-
 function randomLetter() {
     const alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const alphabetLower = "abcdefghijklmnopqrstuvwxyz";
+    
     var rand = Math.floor(Math.random() * (26));
     var randLetterUpper = alphabetUpper[rand];
     var randLetterLower = alphabetLower[rand];
+    
     document.getElementById("letterUpper").innerHTML = randLetterUpper;
     document.getElementById("letterLower").innerHTML = randLetterLower;
 }
